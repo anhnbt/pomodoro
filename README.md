22efea38003a10975aa9e8727195726c39ad571b
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2023-08-28 22:44:52 +0700

    Hoàn thiện version 1.0
    - Sử dụng Bulma làm giao diện
    - Bắt đầu/Tạm dừng Pomodoro
    - Phát audio khi hoàn thành Pomodoro
    - Cho bộ đếm chạy dưới nền ngay cả khi đóng popup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pomodoro
- Tiện ích đồng hồ Pomodoro đếm ngược giúp tập trung học bài trong 25 phút
+Tiện ích Pomodoro - đồng hồ đếm ngược giúp tập trung học bài trong 25 phút
+##
+
+Tiện ích đồng hồ Pomodoro
+• Quãng nghỉ ngắn & dài
+• Icon trên thanh trình duyệt cùng đồng hồ đếm ngược
+• Theo dõi lịch sử Pomodoro & thống kê
+• Quãng nghỉ dài có thể điều chỉnh
+• Đồng hồ đếm ngược có thể điều chỉnh
+• Có thông báo qua máy và tab mới
+• Hơn 20 loại âm báo
+• Có tiếng tíc tắc khi đếm ngược
+• Có thể cài đặt đồng hồ đếm ngược tự động
+• Phần mềm mã nguồn mở
+
+https://github.com/anhnbt/pomodoro
+
+## 
+
+```
+npm install
+npm install -g babel-cli
+npm run js-build
+npm run css-build
+```
---
+++ dist/background.js
@@ -0,0 +1,99 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
+    chrome.tabs.create({
+      url: "https://www.anhnbt.com/ky-tu-dac-biet"
+    });
+  } else if (details.reason == "update") {
+    //call a function to handle an update
+  }
+}), chrome.runtime.setUninstallURL("https://www.anhnbt.com/ky-tu-dac-biet");
+const DEFAULT_POMODORO_MINUTES = 1;
+let timer;
+let minutes;
+let seconds;
+let isTimerRunning = false;
+chrome.storage.local.get(["pomodoroMinutes", "pomodoroSeconds"], result => {
+  minutes = result.pomodoroMinutes || DEFAULT_POMODORO_MINUTES;
+  seconds = result.pomodoroSeconds || 0;
+  updateTimerDisplay(); // Cập nhật giá trị trên giao diện khi tải xong
+  //startBackgroundTimer();
+});
+
+function updateTimerDisplay() {
+  chrome.runtime.sendMessage({ action: "updateDisplay", minutes, seconds });
+}
+
+function startBackgroundTimer() {
+  timer = setInterval(() => {
+    if (seconds > 0) {
+      seconds--;
+    } else {
+      if (minutes > 0) {
+        minutes--;
+        seconds = 59;
+      } else {
+        clearInterval(timer);
+        chrome.runtime.sendMessage({ action: "timerCompleted" });
+        playSound();
+      }
+    }
+    saveTimerToStorage();
+    updateTimerDisplay(); // Cập nhật giá trị trên giao diện sau mỗi tick
+  }, 1000);
+}
+
+function saveTimerToStorage() {
+  chrome.storage.local.set({
+    pomodoroMinutes: minutes,
+    pomodoroSeconds: seconds
+  });
+}
+
+function resetBackgroundTimer() {
+  clearInterval(timer);
+  minutes = DEFAULT_POMODORO_MINUTES;
+  seconds = 0;
+  isTimerRunning = false;
+  saveTimerToStorage();
+  updateTimerDisplay(); // Cập nhật hiển thị sau khi đặt lại thời gian
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "startTimer") {
+    startBackgroundTimer();
+    console.log("startTimer được gọi");
+  } else if (request.action === "stopTimer") {
+    clearInterval(timer);
+    isTimerRunning = false;
+    console.log("stopTimer được gọi");
+    // Thực hiện các xử lý khác cần thiết
+  } else if (request.action === "resetTimer") {
+    resetBackgroundTimer();
+    console.log("resetTimer được gọi");
+  } else if (request.action === "pauseTimer") {
+    clearInterval(timer);
+    isTimerRunning = false;
+    console.log("pauseTimer được gọi");
+    // Thực hiện các xử lý khác cần thiết
+  }
+});
+
+/**
+ * Plays audio files from extension service workers
+ * @param {string} source - path of the audio file
+ * @param {number} volume - volume of the playback
+ */
+async function playSound(source = "audio/casio-watch.mp3", volume = 1) {
+  await createOffscreen();
+  await chrome.runtime.sendMessage({ play: { source, volume } });
+}
+
+// Create the offscreen document if it doesn't already exist
+async function createOffscreen() {
+  if (await chrome.offscreen.hasDocument()) return;
+  await chrome.offscreen.createDocument({
+    url: "offscreen.html",
+    reasons: ["AUDIO_PLAYBACK"],
+    justification: "testing" // details for using the API
+  });
+}
