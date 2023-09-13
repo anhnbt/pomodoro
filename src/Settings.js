73df6f5db0860b5ba000bf0b916f32e5856aa966
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2023-09-13 22:50:18 +0700

    Thêm thông báo lưu cài đặt thành công

--- src/Settings.js
+++ src/Settings.js
@@ -13,6 +13,7 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
+import Snackbar from "@mui/material/Snackbar";
 import { player } from "./player";
 import {
   ALARM_BELL,
@@ -78,6 +79,19 @@ const Settings = forwardRef((props, ref) => {
     currentSettings.autoStartEnabled
   );
 
+  const [state, setState] = React.useState({
+    open: false,
+    vertical: "top",
+    horizontal: "center",
+    msg: "",
+  });
+  
+  const { vertical, horizontal, openSnackbar, msg } = state;
+
+  const handleCloseSnackbar = () => {
+    setState({ ...state, openSnackbar: false });
+  };
+
   const handleSaveSettings = () => {
     // Thực hiện lưu các giá trị cài đặt vào Redux store và Local Storage
     dispatch(setPomodoroTime(newPomodoroTime));
@@ -105,6 +119,12 @@ const Settings = forwardRef((props, ref) => {
     localStorage.setItem("autoStartEnabled", newAutoStartEnabled);
     alarmSound.stop();
     tickingSound.stop();
+    setState({
+      vertical: "bottom",
+      horizontal: "center",
+      openSnackbar: true,
+      msg: "Đã lưu cài đặt.",
+    });
   };
 
   // Truyền ref vào component con
@@ -164,178 +184,191 @@ const Settings = forwardRef((props, ref) => {
   };
 
   return (
-    <FormGroup>
-      <Box sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}>
-        <Box sx={{ my: 1, mx: 1 }}>
-          <Typography variant="h6" gutterBottom>
-            Âm thanh
-          </Typography>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="center"
-            rowSpacing={2}
-            columnSpacing={0}
-          >
-            <Grid item xs={8}>
-              Báo động
+    <div>
+      <FormGroup>
+        <Box sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}>
+          <Box sx={{ my: 1, mx: 1 }}>
+            <Typography variant="h6" gutterBottom>
+              Âm thanh
+            </Typography>
+            <Grid
+              container
+              justifyContent="space-between"
+              alignItems="center"
+              rowSpacing={2}
+              columnSpacing={0}
+            >
+              <Grid item xs={8}>
+                Báo động
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth size="small">
+                  <InputLabel id="alarm-sound=label">Báo động</InputLabel>
+                  <Select
+                    labelId="alarm-sound-label"
+                    id="alarm-sound"
+                    value={newAlarmSound}
+                    label="Âm thanh tích tắc"
+                    onChange={handleChangeAlarmSound}
+                  >
+                    <MenuItem value={"ALARM_BELL"}>Bell</MenuItem>
+                    <MenuItem value={"ALARM_BIRD"}>Bird</MenuItem>
+                    <MenuItem value={"ALARM_DIGITAL"}>Digital</MenuItem>
+                    <MenuItem value={"ALARM_KITCHEN"}>Kitchen</MenuItem>
+                    <MenuItem value={"ALARM_WOOD"}>Wood</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={8}>
+                Tích tắc
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth size="small">
+                  <InputLabel id="ticking-sound=label">Tích tắc</InputLabel>
+                  <Select
+                    labelId="ticking-sound-label"
+                    id="ticking-sound"
+                    value={newTickingSound}
+                    label="Âm thanh tích tắc"
+                    onChange={handleChangeTickingSound}
+                  >
+                    <MenuItem value={"TICKING_NONE"}>
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={"TICKING_FAST"}>Ticking Fast</MenuItem>
+                    <MenuItem value={"TICKING_SLOW"}>Ticking Slow</MenuItem>
+                    <MenuItem value={"WHITE_NOISE"}>White Noise</MenuItem>
+                    <MenuItem value={"BROWN_NOISE"}>Brown Noise</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <FormControl fullWidth size="small">
-                <InputLabel id="alarm-sound=label">Báo động</InputLabel>
-                <Select
-                  labelId="alarm-sound-label"
-                  id="alarm-sound"
-                  value={newAlarmSound}
-                  label="Âm thanh tích tắc"
-                  onChange={handleChangeAlarmSound}
-                >
-                  <MenuItem value={"ALARM_BELL"}>Bell</MenuItem>
-                  <MenuItem value={"ALARM_BIRD"}>Bird</MenuItem>
-                  <MenuItem value={"ALARM_DIGITAL"}>Digital</MenuItem>
-                  <MenuItem value={"ALARM_KITCHEN"}>Kitchen</MenuItem>
-                  <MenuItem value={"ALARM_WOOD"}>Wood</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={8}>
-              Tích tắc
-            </Grid>
-            <Grid item xs={4}>
-              <FormControl fullWidth size="small">
-                <InputLabel id="ticking-sound=label">Tích tắc</InputLabel>
-                <Select
-                  labelId="ticking-sound-label"
-                  id="ticking-sound"
-                  value={newTickingSound}
-                  label="Âm thanh tích tắc"
-                  onChange={handleChangeTickingSound}
-                >
-                  <MenuItem value={"TICKING_NONE"}>
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={"TICKING_FAST"}>Ticking Fast</MenuItem>
-                  <MenuItem value={"TICKING_SLOW"}>Ticking Slow</MenuItem>
-                  <MenuItem value={"WHITE_NOISE"}>White Noise</MenuItem>
-                  <MenuItem value={"BROWN_NOISE"}>Brown Noise</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
-          <Stack spacing={2} direction="row" sx={{ pt: 4 }} alignItems="center">
-            <VolumeDown />
+            <Stack
+              spacing={2}
+              direction="row"
+              sx={{ pt: 4 }}
+              alignItems="center"
+            >
+              <VolumeDown />
+              <Slider
+                aria-label="Default"
+                valueLabelDisplay="auto"
+                value={newVolume}
+                onChange={(e, value) => setNewVolume(value)}
+                min={0}
+                max={1}
+                step={0.01}
+              />
+              <VolumeUp />
+            </Stack>
+          </Box>
+          <Divider variant="middle" />
+
+          <Box sx={{ my: 3, mx: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Thời gian (phút)
+            </Typography>
+            <Typography id="pomodoro-slider" gutterBottom variant="body1">
+              Pomodoro
+            </Typography>
             <Slider
-              aria-label="Default"
+              defaultValue={25}
+              value={newPomodoroTime}
+              min={5}
+              step={5}
+              max={120}
+              onChange={(e) => setNewPomodoroTime(e.target.value)}
               valueLabelDisplay="auto"
-              value={newVolume}
-              onChange={(e, value) => setNewVolume(value)}
-              min={0}
-              max={1}
-              step={0.01}
+              aria-labelledby="pomodoro-slider"
             />
-            <VolumeUp />
-          </Stack>
-        </Box>
-        <Divider variant="middle" />
+            <Typography id="short-break-slider" gutterBottom variant="body1">
+              Nghỉ ngắn
+            </Typography>
+            <Slider
+              defaultValue={5}
+              value={newShortBreakTime}
+              min={5}
+              step={5}
+              max={30}
+              onChange={(e) => setNewShortBreakTime(e.target.value)}
+              valueLabelDisplay="auto"
+              aria-labelledby="short-break-slider"
+            />
+            <Typography id="long-break-slider" gutterBottom variant="body1">
+              Nghỉ dài
+            </Typography>
+            <Slider
+              defaultValue={15}
+              value={newLongBreakTime}
+              min={5}
+              step={5}
+              max={30}
+              onChange={(e) => setNewLongBreakTime(e.target.value)}
+              valueLabelDisplay="auto"
+              aria-labelledby="long-break-slider"
+            />
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item xs={10}>
+                Tự động chạy Pomodoro
+              </Grid>
+              <Grid item xs={2}>
+                <Switch
+                  checked={newAutoStartPomodoroEnabled}
+                  onChange={() =>
+                    setNewAutoStartPomodoroEnabled(!newAutoStartPomodoroEnabled)
+                  }
+                  inputProps={{ "aria-label": "newAutoStartPomodoroEnabled" }}
+                />
+              </Grid>
+              <Grid item xs={10}>
+                Tự động chạy giờ giải lao
+              </Grid>
+              <Grid item xs={2}>
+                <Switch
+                  checked={newAutoStartEnabled}
+                  onChange={() => setNewAutoStartEnabled(!newAutoStartEnabled)}
+                  inputProps={{ "aria-label": "newAutoStartEnabled" }}
+                />
+              </Grid>
+            </Grid>
+          </Box>
+          <Divider variant="middle" />
 
-        <Box sx={{ my: 3, mx: 2 }}>
-          <Typography variant="h6" gutterBottom>
-            Thời gian (phút)
-          </Typography>
-          <Typography id="pomodoro-slider" gutterBottom variant="body1">
-            Pomodoro
-          </Typography>
-          <Slider
-            defaultValue={25}
-            value={newPomodoroTime}
-            min={5}
-            step={5}
-            max={120}
-            onChange={(e) => setNewPomodoroTime(e.target.value)}
-            valueLabelDisplay="auto"
-            aria-labelledby="pomodoro-slider"
-          />
-          <Typography id="short-break-slider" gutterBottom variant="body1">
-            Nghỉ ngắn
-          </Typography>
-          <Slider
-            defaultValue={5}
-            value={newShortBreakTime}
-            min={5}
-            step={5}
-            max={30}
-            onChange={(e) => setNewShortBreakTime(e.target.value)}
-            valueLabelDisplay="auto"
-            aria-labelledby="short-break-slider"
-          />
-          <Typography id="long-break-slider" gutterBottom variant="body1">
-            Nghỉ dài
-          </Typography>
-          <Slider
-            defaultValue={15}
-            value={newLongBreakTime}
-            min={5}
-            step={5}
-            max={30}
-            onChange={(e) => setNewLongBreakTime(e.target.value)}
-            valueLabelDisplay="auto"
-            aria-labelledby="long-break-slider"
-          />
-          <Grid container alignItems="center" spacing={2}>
-            <Grid item xs={10}>
-              Tự động chạy Pomodoro
-            </Grid>
-            <Grid item xs={2}>
-              <Switch
-                checked={newAutoStartPomodoroEnabled}
-                onChange={() =>
-                  setNewAutoStartPomodoroEnabled(!newAutoStartPomodoroEnabled)
-                }
-                inputProps={{ "aria-label": "newAutoStartPomodoroEnabled" }}
-              />
-            </Grid>
-            <Grid item xs={10}>
-              Tự động chạy giờ giải lao
-            </Grid>
-            <Grid item xs={2}>
-              <Switch
-                checked={newAutoStartEnabled}
-                onChange={() =>
-                  setNewAutoStartEnabled(!newAutoStartEnabled)
-                }
-                inputProps={{ "aria-label": "newAutoStartEnabled" }}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-        <Divider variant="middle" />
+          <Box sx={{ my: 3, mx: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Đồng hồ
+            </Typography>
 
-        <Box sx={{ my: 3, mx: 2 }}>
-          <Typography variant="h6" gutterBottom>
-            Đồng hồ
-          </Typography>
-
-          <Grid container alignItems="center" spacing={2}>
-            <Grid item xs={8}>
-              Định dạng giờ
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item xs={8}>
+                Định dạng giờ
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth size="small">
+                  <Select
+                    labelId="hour-format-label"
+                    id="hour-format"
+                    value={newHourFormat}
+                    onChange={(e) => setNewHourFormat(e.target.value)}
+                  >
+                    <MenuItem value={"12"}>12 Giờ</MenuItem>
+                    <MenuItem value={"24"}>24 Giờ</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <FormControl fullWidth size="small">
-                <Select
-                  labelId="hour-format-label"
-                  id="hour-format"
-                  value={newHourFormat}
-                  onChange={(e) => setNewHourFormat(e.target.value)}
-                >
-                  <MenuItem value={"12"}>12 Giờ</MenuItem>
-                  <MenuItem value={"24"}>24 Giờ</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </FormGroup>
+      </FormGroup>
+
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={openSnackbar}
+        onClose={handleCloseSnackbar}
+        message={msg}
+        key={vertical + horizontal}
+      />
+    </div>
   );
 });
 
